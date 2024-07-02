039677f9732ca8c35103ee1e9a3ee53a713be3ef
Yana Suslyakova <susljakovajana@yandex.ru>
2024-07-02 11:01:04 +0300

    fix

--- src/services/fetchProducts.js
+++ src/services/fetchProducts.js
@@ -1,7 +1,6 @@
 export const fetchProducts = async (searchValue, currentPage = 1, productsOnPage = 24) => {
     const skip = productsOnPage * (currentPage - 1);
     const search = searchValue ? `search?q=${searchValue}` : `?limit=${productsOnPage}&skip=${skip}`;
-    console.log(search)
     const response = await fetch(`https://dummyjson.com/products/${search}`);
     if (!response.ok) {
         throw new Error('Failed to fetch products');
